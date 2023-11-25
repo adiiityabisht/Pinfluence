4f89a2254f45c7f0bee1cd9ed9011d67de1473ff
adiiityabisht <adityabisht227@gmail.com>
2023-11-25 11:01:12 +0530

    rightbarComponent

--- src/components/rightbar/Rightbar.jsx
+++ src/components/rightbar/Rightbar.jsx
@@ -1,9 +1,29 @@
-import "./rightbar.css"
+import "./rightbar.css";
 
 const Rightbar = () => {
   return (
-    <div className="rightbar">Rightbar</div>
-  )
-}
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        <div className="birthdayContainer">
+          <img src="assets/gift.png" alt="" className="birthdayImg" />
+          <span className="birthdayText">
+            <b>Parthvi</b> and <b>3 other friends</b> have birthday today
+          </span>
+        </div>
+        <img src="assets/ad.png" alt="" className="rightbarAd" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          <li className="rightbarFriend">
+            <div className="rightbarProfileImgCOntainer">
+              <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg" />
+              <span className="rightbarOnline"></span>
+            </div>
+            <span className="rightbarUsername">Hetav Modi</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
 
-export default Rightbar
+export default Rightbar;
